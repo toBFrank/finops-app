d35d627434ff0a39682d6f6db203cae0299e7780
Franco Bonilla <bonilla.franco484@gmail.com>
2025-08-27 01:31:12 -0600

    hooked up to postgresql

--- moolah-core/src/main/java/com/moolah/moolah_core/models/Portfolio.java
+++ moolah-core/src/main/java/com/moolah/moolah_core/models/Portfolio.java
@@ -2,10 +2,21 @@ package com.moolah.moolah_core.models;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Portfolio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long userId;
     private String symbol;
--- moolah-core/src/main/java/com/moolah/moolah_core/models/User.java
+++ moolah-core/src/main/java/com/moolah/moolah_core/models/User.java
@@ -2,11 +2,22 @@ package com.moolah.moolah_core.models;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class User {
     // Fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long roleId;
     private String username;
