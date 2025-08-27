b2c6d7a5f11c7150bf6a27831e7a931f063f7acc
Franco Bonilla <bonilla.franco484@gmail.com>
2025-08-26 19:14:27 -0600

    Spring boot project setup

---
+++ moolah-core/src/test/java/com/moolah/moolah_core/MoolahCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moolah.moolah_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoolahCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
